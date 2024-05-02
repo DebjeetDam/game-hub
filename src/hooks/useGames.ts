ff57da14db2a4935cfce838655d04efc2d606140
DebjeetDam <debjeet.dam95@gmail.com>
2024-05-01 20:52:51 -0500

    getting metacritic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
   background_image: string;
   // parent_platforms:[{platform: {id: 1, name: "PC", slug: "pc"}},…]
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
