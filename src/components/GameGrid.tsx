d0c4a4062528b17634b157c372f301704f3e680d
DebjeetDam <debjeet.dam95@gmail.com>
2024-05-02 16:45:36 -0500

    removing fixed width to game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,11 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       {/* column sizes for devices */}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={3}
+      >
         {isLoading &&
           skeletons.map((x) => (
             <GameCardContainer>
